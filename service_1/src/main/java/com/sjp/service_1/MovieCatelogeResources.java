2713729110833599e97ffd93f32ab2627eaf8f59
Emindu <emindu.perera@gmail.com>
2021-04-12 13:59:30 +0530

    connect with eureke server. service_1

--- service_1/src/main/java/com/sjp/service_1/MovieCatelogeResources.java
+++ service_1/src/main/java/com/sjp/service_1/MovieCatelogeResources.java
@@ -3,6 +3,7 @@ package com.sjp.service_1;
 import com.sjp.service_1.beans.CatelogItem;
 import com.sjp.service_1.beans.Movie;
 import com.sjp.service_1.beans.Rating;
+import com.sjp.service_1.beans.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,17 +34,19 @@ public class MovieCatelogeResources {
         //get all rated movie IDs
         //for each movie ID, call movie info service and get details
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3 )
-        );
+//        List<Rating> ratings = Arrays.asList(
+//                new Rating("1234", 4),
+//                new Rating("5678", 3 )
+//        );
 
-        return ratings.stream().map(rating -> {
+        UserRating ratings = restTemplate.getForObject("http://RATING-INFO/ratingsdata/users/" + userId, UserRating.class);
+
+        return ratings.getRatings().stream().map(rating -> {
             //depreacted
 
             //Movie movie = restTemplate.getForObject("http://localhost:8085/movies/"+ rating.getMovieId(), Movie.class);
             Movie movie = webClientBuilder.build().get()
-                    .uri("http://localhost:8085/movies/"+ rating.getMovieId())
+                    .uri("http://MOVIE-INFO/movies/"+ rating.getMovieId())
                     .retrieve()
                     .bodyToMono(Movie.class)
                     .block();
--- service_1/src/main/java/com/sjp/service_1/Service1Application.java
+++ service_1/src/main/java/com/sjp/service_1/Service1Application.java
@@ -2,20 +2,25 @@ package com.sjp.service_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Service1Application {
 
     @Bean
+    @LoadBalanced //use to tell client to do client side load balancing
     public RestTemplate getrestTemplate(){
         return new RestTemplate();
     }
 
 
     @Bean
+    @LoadBalanced
     public WebClient.Builder getWebClientBuilder(){
         return WebClient.builder();
     }
