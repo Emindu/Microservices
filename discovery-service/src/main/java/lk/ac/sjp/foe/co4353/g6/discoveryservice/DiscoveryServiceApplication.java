962fb4e6793c8b445a79fd32240c08d9ef9421c3
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-04-24 18:32:00 +0530

    refactor(common): fix a typo in package naming

---
+++ discovery-service/src/main/java/lk/ac/sjp/foe/co4353/g6/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package lk.ac.sjp.foe.co4353.g6.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ reputation-service/src/test/java/lk/ac/sjp/foe/co4353/g6/reputationservice/ReputationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sjp.foe.co4353.g6.reputationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReputationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
