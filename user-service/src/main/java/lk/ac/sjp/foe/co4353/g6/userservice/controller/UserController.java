acde504280cd7ad20d9e1ae0e20e436a09eb2ca0
Ravindu Sandeepa <ravindursr@gmail.com>
2021-05-16 02:05:16 +0530

    feat(user-service): add endpoint to get user reputation

--- user-service/src/main/java/lk/ac/sjp/foe/co4353/g6/userservice/controller/UserController.java
+++ user-service/src/main/java/lk/ac/sjp/foe/co4353/g6/userservice/controller/UserController.java
@@ -1,6 +1,6 @@
 package lk.ac.sjp.foe.co4353.g6.userservice.controller;
 
-import lk.ac.sjp.foe.co4353.g6.userservice.model.User;
+import lk.ac.sjp.foe.co4353.g6.userservice.model.*;
 import lk.ac.sjp.foe.co4353.g6.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -54,6 +54,17 @@ public class UserController {
         try {
             User _user = userRepository.save(user);
             return new ResponseEntity<>(_user, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("reputation/{userId}")
+    public ResponseEntity<Reputation> getUserReputation(@PathVariable long userId){
+
+        try {
+            Reputation reputation = userRepository.findReputationByUserId(userId);
+            return new ResponseEntity<>(reputation, HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- user-service/src/main/java/lk/ac/sjp/foe/co4353/g6/userservice/repository/UserRepository.java
+++ user-service/src/main/java/lk/ac/sjp/foe/co4353/g6/userservice/repository/UserRepository.java
@@ -1,12 +1,18 @@
 package lk.ac.sjp.foe.co4353.g6.userservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import lk.ac.sjp.foe.co4353.g6.userservice.model.User;
+import lk.ac.sjp.foe.co4353.g6.userservice.model.*;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserId(long userId);
+
     List<User> findAll();
+
+    @Query("SELECT new lk.ac.sjp.foe.co4353.g6.userservice.model.Reputation(u.userId, u.reputation) FROM User u where u.userId = :userId")
+    Reputation findReputationByUserId(@Param("userId") long userId);
 }
