bf4ec17a45bdc6ece9babc020ddff4b98996b4e5
Emindu <emindu.perera@gmail.com>
2021-05-15 17:57:00 +0530

    fix(vote-service): added eureka client

--- vote-service/src/main/java/lk/ac/sjp/foe/co4353/g6/voteservice/VoteServiceApplication.java
+++ vote-service/src/main/java/lk/ac/sjp/foe/co4353/g6/voteservice/VoteServiceApplication.java
@@ -7,12 +7,13 @@ import lk.ac.sjp.foe.co4353.g6.voteservice.repository.QuestionVotesRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.Stream;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VoteServiceApplication {
 
     public static void main(String[] args) {
