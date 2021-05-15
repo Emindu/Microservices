b6d544b5d0b2c1afa88c49cea72b957b8df38b15
Ravindu Sandeepa <ravindursr@gmail.com>
2021-05-15 23:49:25 +0530

    feat(user-service): add reputation property to user model

--- user-service/src/main/java/lk/ac/sjp/foe/co4353/g6/userservice/model/User.java
+++ user-service/src/main/java/lk/ac/sjp/foe/co4353/g6/userservice/model/User.java
@@ -16,13 +16,17 @@ public class User {
     @Column(name="country")
     private String country;
 
+    @Column(name="reputation")
+    private int reputation;
+
     public User(){
 
     }
 
-    public User(String name, String country) {
+    public User(String name, String country, int reputation) {
         this.name = name;
         this.country = country;
+        this.reputation = reputation;
     }
 
     public long getUserId() {
@@ -45,7 +49,9 @@ public class User {
         return country;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+    public void setCountry(String country) { this.country = country; }
+
+    public int getReputation() { return reputation; }
+
+    public void setReputation(int reputation){ this.reputation = reputation; }
 }
