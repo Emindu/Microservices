b16908d040c1253d3eca4909a9a43daa7d93a54b
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-05-16 10:20:18 +0530

    feat(api-gateway): add basic configs

--- zuul-gateway/src/main/java/lk/ac/sjp/foe/co4353/g6/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/lk/ac/sjp/foe/co4353/g6/zuulgateway/ZuulGatewayApplication.java
@@ -3,9 +3,11 @@ package lk.ac.sjp.foe.co4353.g6.zuulgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
