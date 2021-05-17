c2572ab277e2240724dac394b329439ca674e609
Ravindu Sandeepa <ravindursr@gmail.com>
2021-05-17 10:35:37 +0530

    test(user-service): configure initialization of userdb with test values

--- user-service/src/main/java/lk/ac/sjp/foe/co4353/g6/userservice/UserServiceApplication.java
+++ user-service/src/main/java/lk/ac/sjp/foe/co4353/g6/userservice/UserServiceApplication.java
@@ -1,13 +1,31 @@
 package lk.ac.sjp.foe.co4353.g6.userservice;
 
+import lk.ac.sjp.foe.co4353.g6.userservice.model.User;
+import lk.ac.sjp.foe.co4353.g6.userservice.repository.UserRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner init(UserRepository repository) {
+        return args -> Stream.of(
+                new User(
+                       "Mark snowden", "United States", 20
+                ),
+                new User(
+                        "Tharaka Amarathunga", "Sri Lanka", 30
+                )
+        ).forEach(repository::save);
     }
 }
