5b02c3789c4859c6cb5799f37b3e9760356611bc
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-06-23 12:05:45 +0530

    refactor(frontend): add basic styles

--- frontend/src/app/core/models/Answer.ts
+++ frontend/src/app/core/models/Answer.ts
@@ -4,3 +4,7 @@ import {Post} from "./Post";
 export interface Answer extends Post{
   question: Question;
 }
+
+export function instanceOfAnswer(post: Post): post is Answer {
+  return 'question' in post;
+}
--- frontend/src/app/core/models/Question.ts
+++ frontend/src/app/core/models/Question.ts
@@ -4,3 +4,7 @@ export interface Question extends Post{
   title: string;
   answersCount: number,
 }
+
+export function instanceOfQuestion(post: Post): post is Question {
+  return 'title' in post;
+}
